49bcdc950ab01a82463b271a16cc49ad98d6df0b
Tooni <prashanth04@gmail.com>
2022-10-05 01:41:34 +0530

    Models and Admin

--- djCRM/leads/admin.py
+++ djCRM/leads/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from leads.models import User, Agent, Lead
 
 # Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    model: User
+
+
+class AgentAdmin(admin.ModelAdmin):
+    model: Agent
+
+
+class LeadAdmin(admin.ModelAdmin):
+    model: Lead
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Agent, AgentAdmin)
+admin.site.register(Lead, LeadAdmin)
