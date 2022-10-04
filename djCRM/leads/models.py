84f11685a0b67382a881bc1afb72f6de8b2af587
Tooni <prashanth04@gmail.com>
2022-10-05 01:00:53 +0530

    Added Custom User Model

--- djCRM/leads/models.py
+++ djCRM/leads/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class Agent(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
+class User(AbstractUser):
+    pass
 
 
 class Lead(models.Model):
@@ -11,3 +12,7 @@ class Lead(models.Model):
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
     agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
